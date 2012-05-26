ff51842a8c11567ae814bc77433e0de464573ad4
Vangos Pterneas <pterneas@gmail.com>
2012-05-26 18:28:44 +0300

    First release

---
+++ Ribbon.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace KinectPowerPoint
+{
+    partial class Ribbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ribbon));
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.grpKinect = this.Factory.CreateRibbonGroup();
+            this.btnPlay = this.Factory.CreateRibbonToggleButton();
+            this.cbxLeftHanded = this.Factory.CreateRibbonCheckBox();
+            this.tab1.SuspendLayout();
+            this.grpKinect.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.grpKinect);
+            this.tab1.Label = "Kinect";
+            this.tab1.Name = "tab1";
+            // 
+            // grpKinect
+            // 
+            this.grpKinect.Items.Add(this.btnPlay);
+            this.grpKinect.Items.Add(this.cbxLeftHanded);
+            this.grpKinect.Label = "Kinect";
+            this.grpKinect.Name = "grpKinect";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btnPlay.Image = ((System.Drawing.Image)(resources.GetObject("btnPlay.Image")));
+            this.btnPlay.Label = "Start";
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.ShowImage = true;
+            this.btnPlay.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Play_Click);
+            // 
+            // cbxLeftHanded
+            // 
+            this.cbxLeftHanded.Label = "Left handed";
+            this.cbxLeftHanded.Name = "cbxLeftHanded";
+            this.cbxLeftHanded.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.LeftHanded_Click);
+            // 
+            // Ribbon
+            // 
+            this.Name = "Ribbon";
+            this.RibbonType = "Microsoft.PowerPoint.Presentation";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.grpKinect.ResumeLayout(false);
+            this.grpKinect.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup grpKinect;
+        internal Microsoft.Office.Tools.Ribbon.RibbonToggleButton btnPlay;
+        internal Microsoft.Office.Tools.Ribbon.RibbonCheckBox cbxLeftHanded;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon Ribbon
+        {
+            get { return this.GetRibbon<Ribbon>(); }
+        }
+    }
+}
---
+++ Ribbon.cs
@@ -0,0 +1,123 @@
+﻿//
+// Developed by Vangos Pterneas
+// http://vangos.eu
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Kinect;
+using Microsoft.Samples.Kinect.SwipeGestureRecognizer;
+using KinectPowerPoint.Properties;
+
+namespace KinectPowerPoint
+{
+    public partial class Ribbon
+    {
+        KinectSensor _sensor;
+
+        Skeleton[] _skeletons = new Skeleton[6];
+
+        Recognizer _gestureRecognizer = new Recognizer();
+
+        bool _isPlaying = false;
+        bool _isLeftHanded = false;
+
+        private void Ribbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            InitializePowerPoint();
+            InitializeKinect();
+        }
+
+        private void InitializePowerPoint()
+        {
+            
+        }
+
+        private void InitializeKinect()
+        {
+            _sensor = KinectSensor.KinectSensors.Where(sensor => sensor.Status == KinectStatus.Connected).FirstOrDefault();
+
+            if (_sensor != null)
+            {
+                _sensor.SkeletonStream.Enable();
+                _sensor.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(Sensor_SkeletonFrameReady);
+
+                _gestureRecognizer.SwipeLeftDetected += new EventHandler<KinectGestureEventArgs>(GestureRecognizer_SwipeLeftDetected);
+                _gestureRecognizer.SwipeRightDetected += new EventHandler<KinectGestureEventArgs>(GestureRecognizer_SwipeRightDetected);
+
+                btnPlay.Enabled = true;
+            }
+            else
+            {
+                grpKinect.Label = "Kinect sensor not connected.";
+
+                btnPlay.Enabled = false;
+            }
+        }
+
+        void Sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (!_sensor.IsRunning) return;
+
+            using (var frame = e.OpenSkeletonFrame())
+            {
+                if (frame != null)
+                {
+                    frame.CopySkeletonDataTo(_skeletons);
+
+                    _gestureRecognizer.Recognize(sender, frame, _skeletons);
+                }
+            }
+        }
+
+        void GestureRecognizer_SwipeLeftDetected(object sender, KinectGestureEventArgs e)
+        {
+            if (!_sensor.IsRunning) return;
+
+            var slideshow = Globals.ThisAddIn.Application.ActivePresentation.SlideShowWindow.View;
+
+            if (!_isLeftHanded)
+            {
+                slideshow.Previous();
+            }
+            else
+            {
+                slideshow.Next();
+            }
+        }
+
+        void GestureRecognizer_SwipeRightDetected(object sender, KinectGestureEventArgs e)
+        {
+            if (!_sensor.IsRunning) return;
+
+            var slideshow = Globals.ThisAddIn.Application.ActivePresentation.SlideShowWindow.View;
+
+            if (!_isLeftHanded)
+            {
+                slideshow.Next();
+            }
+            else
+            {
+                slideshow.Previous();
+            }
+        }
+
+        private void Play_Click(object sender, RibbonControlEventArgs e)
+        {
+            btnPlay.Image = _isPlaying ? Resources.Play : Resources.Stop;
+            btnPlay.Label = _isPlaying ? "Start" : "Stop";
+
+            if (_isPlaying) _sensor.Stop(); else _sensor.Start();
+
+            _isPlaying = !_isPlaying;
+        }
+
+        private void LeftHanded_Click(object sender, RibbonControlEventArgs e)
+        {
+            _isLeftHanded = cbxLeftHanded.Checked;
+        }
+    }
+}
